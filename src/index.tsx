830e6d552c86b642ee628b61b2f849a8d123a9e4
Tom Long <haoyitedaniu@gmail.com>
2020-10-08 22:06:27 -0700

    Tom Long tested myHeaderDiv i.e. camelCase

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 
 import styles from "./style/app.scss";
 
-ReactDOM.render(<h1 className={styles.header}>Hello world</h1>, document.getElementById("root"));
+console.log(styles);
+
+ReactDOM.render(
+  <h1
+    className={
+      // styles.header
+      styles.myHeaderDiv
+    }
+  >
+    Hello world
+  </h1>,
+  document.getElementById("root")
+);
